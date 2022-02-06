d2749c9f2c339eff59635f23bf192959f98367e5
Christos Chronis <it21797@hua.gr>
2022-02-06 21:10:11 +0200

    Correction on Dockerfiles for x86/x64 docker images build

--- basic_mages/create_images.sh
+++ basic_mages/create_images.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+echo "Start building Basic Image"
 docker build -f Dockerfile.basic_image . -t teaching_image
+echo "Basic Image Finished!"
+echo "Start building Tensorflow Image"
 docker build -f Dockerfile.basic_image_Tensorflow . -t teaching_image_tensorflow
+echo "Tensorflow Image Finished!"
