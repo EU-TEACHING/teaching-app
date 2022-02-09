20d6d1da10bd56ccd6aa666573b2cd98d4dd4446
Christos Chronis <it21797@hua.gr>
2022-02-09 10:25:39 +0200

    Image for RTMP Server with arm and x86/x64  support

--- basic_images/create_ARM_images.sh
+++ basic_images/create_ARM_images.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
+echo "Start building Basic Image"
 docker build -f Dockerfile.basic_image_ARM . -t teaching_image
+echo "Basic Image Finished!"
+echo "Start building Tensorflow Image"
 docker build -f Dockerfile.basic_image_Tensorflow_ARM . -t teaching_image_tensorflow
+echo "Tensorflow Image Finished!"
+echo "Create Image for video server"
+docker pull ajeetraina/nginx-rtmp-arm
+docker image tag ajeetraina/nginx-rtmp-arm rtmp_server
+echo "Video server Finished!"
--- basic_images/create_images.sh
+++ basic_images/create_images.sh
@@ -5,3 +5,7 @@ echo "Basic Image Finished!"
 echo "Start building Tensorflow Image"
 docker build -f Dockerfile.basic_image_Tensorflow . -t teaching_image_tensorflow
 echo "Tensorflow Image Finished!"
+echo "Create Image for video server"
+docker pull tiangolo/nginx-rtmp
+docker image tag tiangolo/nginx-rtmp rtmp_server
+echo "Video server Finished!"
