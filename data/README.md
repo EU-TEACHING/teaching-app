7a03ac24f2081a45afda73bb779cb1c5fecce110
vdecaro <valeriodc95@hotmail.it>
2022-05-18 19:19:12 +0200

    Submodules removed

--- data/README.md
+++
@@ -1,2 +0,0 @@
-# data
-Project for persistent storage, e.g., the influxdb instance.
--- data/main.py
+++
@@ -1,8 +0,0 @@
-import os
-from pydoc import locate
-
-
-if __name__ == '__main__':
-    service_class = locate(os.environ['SERVICE_TYPE'])
-    service = service_class()
-    service()
